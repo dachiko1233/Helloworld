afcb6bfae051356597b22eca337a8f36c4364168
dachiko1233 <dachimari9@email.com>
2026-05-10 21:05:14 +0300

    Hello

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World",
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
 		})
 	})
-	r.Run(":8081") // listen and serve on 0.0.0.0:8081
+	router.Run(":8080")
 }
